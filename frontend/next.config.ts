2638d9aa365af9926386113d1ba731f95605f36d
Yuri Garcia <yuripc55@gmail.com>
2025-12-12 23:30:03 -0300

    corrigindoerro404

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: false,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
