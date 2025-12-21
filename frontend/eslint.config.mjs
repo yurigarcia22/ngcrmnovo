4d77a575d1adf39a2c792fde793196abf7f5303a
Yuri Garcia <yuripc55@gmail.com>
2025-12-21 19:00:40 -0300

    remoção middleware

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -3,7 +3,7 @@ import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = dirname(__filename); // ESM polyfill for Vercel
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
