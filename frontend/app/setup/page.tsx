67d8ae7ecce8ae59614de83f95b6c16d04001ef5
Yuri Garcia <yuripc55@gmail.com>
2025-12-21 17:55:18 -0300

    Correção de erros

--- frontend/app/setup/page.tsx
+++ frontend/app/setup/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useEffect, useState } from "react";
 import { createClient } from "@/utils/supabase/client";
 import { useRouter } from "next/navigation";
